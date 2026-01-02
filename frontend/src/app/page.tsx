0c0a42e134f2463802310ded58535b6951c7928b
Himanshujadhav2004 <himanshujadhav341@gmail.com>
2026-01-02 16:23:12 +0530

    completed the inngest setup and integrated with the trpc

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,25 +1,24 @@
 
-import { caller, getQueryClient, trpc } from "@/trpc/server";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { Client } from "./client";
-import { Suspense } from "react";
+
+"use client"
+import { useTRPC } from "@/trpc/client"
+import { useMutation } from "@tanstack/react-query";
 
 
- const page= async()=> {  
-const queryClient = getQueryClient();
-void queryClient.prefetchQuery(trpc.createAi.queryOptions({text:"Himanshu"}))
+
+
+ const page=()=> {  
+
+  const trpc = useTRPC();
+  const invoke= useMutation(trpc.invoke.mutationOptions({}));
+
 console.log("Server componet")
 
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-{/* { JSON.stringify(data)} */}
-   
-   <Suspense fallback={<p>loading...</p>}>
-<Client></Client>
-</Suspense>
-    </div>
-    </HydrationBoundary>
+ <div>
+  <button onClick={()=> invoke.mutate({text:"himanshu"})}>Invoke Background Job</button>
+
+ </div>
   );
 }
 
